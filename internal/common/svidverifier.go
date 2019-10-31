80ec8558b354be4979f148322161f59fd5173ccb
Dennis Gove <dgove1@bloomberg.net>
2019-10-31 11:00:08 -0400

    Fixes #7: Properly documents and implements expectation the location of SPIFFE ID in X509 SVID
    Signed-off-by: Dennis Gove <dgove1@bloomberg.net>

--- internal/common/svidverifier.go
+++ internal/common/svidverifier.go
@@ -42,19 +42,44 @@ func (verifier *SvidVerifier) AddTrustSource(source TrustSource) {
 	verifier.trustSources = append(verifier.trustSources, source)
 }
 
-// Verify will take the provided SVID and verify its source against any of the known sources of trust.
-// If the SVID was generated using any of the known sources of trust then the SVID will be considered
-// verified and all the certificates found inside the SVID will be returned. If the SVID cannot be
-// verified then an error will be returned.
-func (verifier *SvidVerifier) Verify(svid string) ([]*x509.Certificate, error) {
-
+// VerifyAndExtractSpiffeId will take the provided SVID and verify its source against any of the known
+// sources of trust. If the SVID was generated using any of the known sources of trust and adheres to
+// all required SPIFFE requirements for an SVID then the SVID will be considered verified and the
+// SPIFFE ID of the SVID will be returned. If the SVID cannot be verified then an error will
+// be returned.
+func (verifier *SvidVerifier) VerifyAndExtractSpiffeId(svid string) (string, error) {
 	logrus.Debug("Beginning SVID verification")
 
-	logrus.Debug("Extracting certificates from provided SVID")
-	svidCerts := ExtractCertificatesFromPem([]byte(svid))
-	if len(svidCerts) == 0 {
-		return nil, errors.New("SVID is invalid - no valid certificates found")
+	// right now we only support X509 verification
+	spiffeId, err := verifier.verifyAndExtractSpiffeIdFromX509(svid)
+	if err != nil {
+		return "", err
 	}
+
+	// SPIFFE validation requirements
+	if err := spiffe.ValidateID(spiffeId, spiffe.AllowAnyTrustDomainWorkload()); err != nil {
+		return "", errors.New("SVID is invalid - invalid SPIFFE ID found - " + err.Error())
+	}
+
+	return spiffeId, nil
+}
+
+// VerifyAndExtractSpiffeIdFromX509 will take the provided X509-SVID and verify its source against any
+// of the known sources of trust. If the SVID was generated using any of the known sources of trust
+// and adheres to all required SPIFFE requirements for an X509-SVID then it will be considered verified
+// and the SPIFFE ID found in the SVID will be returned. If the SVID cannot be verified then an error will
+// be returned. The SPIFFE ID of an X509-SVID is defined as the first SAN URI found in the first certificate
+// of the SVID. That is, if it contains multiple certificates then the first is the one containing the
+// 'caller' SPIFFE ID.
+func (verifier *SvidVerifier) verifyAndExtractSpiffeIdFromX509(svid string) (string, error) {
+	logrus.Debug("Beginning SVID X509 verification")
+
+	logrus.Debug("Extracting certificates from provided SVID")
+	svidCertChain := ExtractCertificatesFromPem([]byte(svid))
+	if len(svidCertChain) == 0 {
+		return "", errors.New("SVID is invalid - no valid certificates found")
+	}
+	svidPrincipalCert := svidCertChain[0]
 
 	logrus.Debug("Building map of domains -> trusted certificate pools")
 	trustedCertificatePools := make(map[string]*x509.CertPool, 0)
@@ -71,12 +96,16 @@ func (verifier *SvidVerifier) Verify(svid string) ([]*x509.Certificate, error) {
 		}
 	}
 
-	_, err := spiffe.VerifyPeerCertificate(svidCerts, trustedCertificatePools, spiffe.ExpectAnyPeer())
+	_, err := spiffe.VerifyPeerCertificate(svidCertChain, trustedCertificatePools, spiffe.ExpectAnyPeer())
 	if nil != err {
-		return nil, errors.New("Unable to validate SVID against trust chain - " + err.Error())
+		return "", errors.New("Failed to validate SVID against trust chain - " + err.Error())
 	}
 
-	return svidCerts, nil
+	if len(svidPrincipalCert.URIs) == 0 {
+		return "", errors.New("SVID is invalid - no SPIFFE ID found")
+	}
+
+	return svidPrincipalCert.URIs[0].String(), nil
 }
 
 // ExtractCertificatesFromPem takes an array of bytes representing a PEM file
@@ -84,6 +113,10 @@ func (verifier *SvidVerifier) Verify(svid string) ([]*x509.Certificate, error) {
 // in this method has been borrowed from x509.CertPool::AppendCertsFromPEM which
 // accepts the same argument, extracts, and appends the certificates to an
 // x509.CertPool.
+// The SPIFFE Workload API, reiterating what is declared in the TLS RFC, states
+// that the order of the certificates in the PEM file is important and as such
+// this method will ensure the order of certificates will be preserved.
+// https://github.com/spiffe/spiffe/blob/master/standards/SPIFFE_Workload_API.md#54-profile-messages
 func ExtractCertificatesFromPem(pemCerts []byte) []*x509.Certificate {
 	var certificates []*x509.Certificate
 
